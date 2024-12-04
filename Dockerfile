44d22e286f19a184c5fdd891a3f17e71cab8c99f
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2024-12-05 00:21:22 +0700

    feat: add fetch current user

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,12 @@ FROM node:20-bookworm
 
 WORKDIR /app
 
-# Copy package.json and install dependencies
 COPY package.json package-lock.json ./
 RUN npm install
 
-# Copy source files
 COPY . .
 
-# Expose port
 EXPOSE 3000
 
-# Start Express server
 CMD ["npm", "start"]
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const { Pool } = require('pg');
 
 const HOST = process.env.HOST || 'localhost';
@@ -14,13 +15,37 @@ const POSTGRES_PORT = process.env.POSTGRES_PORT;
 
 const POSTGRES_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
 
 const pool = new Pool({
   connectionString: POSTGRES_URL,
 });
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, JWT_SECRET, async (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    
+    try {
+      const { rows } = await pool.query('SELECT id, username FROM users WHERE id = $1', [decoded.userId]);
+      if (rows.length === 0) return res.sendStatus(404); // User not found
+      
+      req.user = rows[0];
+      next();
+    } catch (dbError) {
+      console.error(dbError);
+      res.sendStatus(500);
+    }
+  });
+};
 
 app.post('/register', async (req, res) => {
   const { username, password, confirmPassword } = req.body;
@@ -33,7 +58,7 @@ app.post('/register', async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
   await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
-  return res.status(201).json({ message: 'User registered' });
+  return res.status(201).json({ statusCode: 201, message: 'User registered' });
 });
 
 app.post('/login', async (req, res) => {
@@ -46,7 +71,22 @@ app.post('/login', async (req, res) => {
   if (!isPasswordValid) return res.status(400).json({ statusCode: 400, message: 'The email address or password entered is incorrect.' });
 
   const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
-  return res.json({ token, user: { ...user, password: undefined } });
+  return res.json({
+    statusCode: 201,
+    message: "User logged in successfuly.",
+    data: {
+      token: token,
+      user: { ...user, password: undefined }
+    },
+  });
+});
+
+app.get('/user', authenticateToken, (req, res) => {
+  return res.json({
+    statusCode: 200,
+    message:  'User fetched successfully.',
+    data: { ...req.user, password: undefined }
+  })
 });
 
 
